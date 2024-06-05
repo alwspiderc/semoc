ec65c91b5c63d17cea1c1335c735e6afc956e184
Ester Costa de Souza <alwesterc@gmail.com>
2024-06-05 09:53:29 -0300

    hide actionbar

--- app/src/main/java/br/com/ucsal/semoc/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/ucsal/semoc/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import br.com.ucsal.semoc.databinding.FragmentHomeBinding
@@ -27,6 +28,8 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        (activity as AppCompatActivity).supportActionBar?.hide()
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
